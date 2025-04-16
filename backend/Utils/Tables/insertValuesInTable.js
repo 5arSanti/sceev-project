403c500b2d13d275e32fad49f2752f02175ef4aa
5arSanti <santiari05@hotmail.com>
2025-04-16 15:55:41 -0500

    feat: Enhance multer configuration to include file type validation for Excel files

--- backend/Utils/Tables/insertValuesInTable.js
+++ backend/Utils/Tables/insertValuesInTable.js
@@ -1,25 +1,29 @@
 const { getQuery } = require("../../database/query");
 
 const insertValuesInTable = async (tableName, correctRows) => {
-
 	if (!correctRows || correctRows.length === 0) {
 		return;
 	}
 
 	const keys = Object.keys(correctRows[0].data).join(", ");
+	const batchSize = 1000;
 
-	const values = correctRows.map((item) => {
-		return `(${Object.values(item.data)
-			.map((value) => (typeof value === "string" ? `'${value}'` : value))
-			.join(", ")})`;
-	}).join(", ");
+	for (let i = 0; i < correctRows.length; i += batchSize) {
+		const batch = correctRows.slice(i, i + batchSize);
 
-	const query = `
-		INSERT INTO ${tableName} (${keys}) VALUES
-		${values}
-	`;
+		const values = batch.map((item) => {
+			return `(${Object.values(item.data)
+				.map((value) => (typeof value === "string" ? `'${value}'` : value))
+				.join(", ")})`;
+		}).join(", ");
 
-	return await getQuery(query);
+		const query = `
+            INSERT INTO ${tableName} (${keys}) VALUES
+            ${values}
+        `;
+
+		await getQuery(query);
+	}
 };
 
 module.exports = { insertValuesInTable };

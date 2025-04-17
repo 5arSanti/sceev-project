4fecdcc11579ab7fc4bd5b6098dc232d3008b9ec
5arSanti <santiari05@hotmail.com>
2025-04-17 11:48:06 -0500

    feat: Add initial test setup with conftest.py, driver factory, and pytest configuration

---
+++ tests/utils/driver_factory.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def create_driver():
+    opts = Options()
+    # opts.add_argument("--headless")
+    opts.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=opts)
+    driver.implicitly_wait(5)
+    return driver

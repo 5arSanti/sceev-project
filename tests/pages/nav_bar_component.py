1c8fd0465d916bf959f50d1f2ce218899c132500
Juan Marquez <juanp-marqueza@unilibre.edu.co>
2025-04-24 23:57:25 -0500

    feat: login automatization testing flows

--- tests/pages/nav_bar_component.py
+++ tests/pages/nav_bar_component.py
@@ -2,9 +2,26 @@ from selenium.webdriver.common.by import By
 
 class NavbarComponent:
     NAV_HOME_BTN = (By.CSS_SELECTOR, "a.nav-buttons.animacion2[href='#/home']")
-    
+    NAV_STATS_BTN = (By.CSS_SELECTOR, "a.nav-buttons.animacion2[href='#/stats']")
+    NAV_LOGIN_BTN = (By.CSS_SELECTOR, "a.nav-buttons.animacion2[href='#/login']")
+    NAV_UPLOAD_BTN = (By.CSS_SELECTOR, "a.nav-buttons.animacion2[href='#/upload']")
+    NAV_USERS_BTN = (By.CSS_SELECTOR, "a.nav-buttons.animacion2[href='#/users']")
+ 
+
     def __init__(self, driver):
         self.driver = driver
         
     def go_back_home(self):
         self.driver.find_element(*self.NAV_HOME_BTN).click()
+
+    def go_stats(self):
+        self.driver.find_element(*self.NAV_STATS_BTN).click()
+
+    def go_login(self):
+        self.driver.find_element(*self.NAV_LOGIN_BTN).click()
+
+    def go_upload(self):
+        self.driver.find_element(*self.NAV_UPLOAD_BTN).click()
+
+    def go_users(self):
+        self.driver.find_element(*self.NAV_USERS_BTN).click()

0569d5189ddba25dc8d8f00e1a7e03b67c7a4ea2
5arSanti <santiari05@hotmail.com>
2025-05-31 15:49:47 -0500

    feat: Add Jest and Supertest for backend testing; update package scripts for testing

---
+++ frontend/jest.config.js
@@ -0,0 +1,21 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/main.jsx',
+    '!src/vite-env.d.ts',
+    '!**/node_modules/**',
+    '!**/coverage/**'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true
+}; 

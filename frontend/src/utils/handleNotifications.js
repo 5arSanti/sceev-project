7bf48e3941e6e797f1e0812d1ff7be9b2dbf2118
Santiago Arias <santiari05@hotmail.com>
2025-06-08 10:01:27 -0500

    refactor: Update notification position default and clean up CSS styles
    - Changed the default notification position from 'top-right' to 'bottom-right' in handleNotifications.js for improved user experience.
    - Added spacing in styles.css for better readability and consistency, while ensuring hover effects maintain their intended design.

--- frontend/src/utils/handleNotifications.js
+++ frontend/src/utils/handleNotifications.js
@@ -1,10 +1,10 @@
 import { toast } from "react-toastify";
 
-const handleNotifications = (type, value, position = 'top-right') => {
+const handleNotifications = (type, value, position = 'bottom-right') => {
     const notiType = {
-        "info": () => toast.info(value || "Información", {position}),
-        "error": () => toast.error(value || "Error", {position}),
-        "success": () => toast.success(value || "Completado Exitosamente", {position}),
+        "info": () => toast.info(value || "Información", { position }),
+        "error": () => toast.error(value || "Error", { position }),
+        "success": () => toast.success(value || "Completado Exitosamente", { position }),
     };
     return (notiType[type] || notiType["info"])();
 }

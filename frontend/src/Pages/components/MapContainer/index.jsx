9a52ef516805229378215602ef044fdf9a4794dc
Santiago Arias <santiari05@hotmail.com>
2025-10-07 23:51:07 -0500

    refactor: Simplify backend scripts and enhance MapContainer layout
    - Updated backend package.json to streamline development and production commands by removing the explicit use of the .env file.
    - Modified MapContainer component to adjust the layout by adding a flexDirection property to WrapperContainer2 for improved styling.
    - Added static positioning to the tool-tip container in styles.css for better responsiveness on smaller screens.

--- frontend/src/Pages/components/MapContainer/index.jsx
+++ frontend/src/Pages/components/MapContainer/index.jsx
@@ -24,7 +24,7 @@ const MapContainer = ({ selectedDepartment }) => {
     }
 
     return(
-        <WrapperContainer2 padding={0}>
+        <WrapperContainer2 padding={0} flexDirection="column">
             <ToolTipMap hoveredDepartment={hoveredDepartment} selectedDepartment={selectedDepartment}/>
 
             <MapCard 
